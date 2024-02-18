a15bede6e800bd934d12b237896bc978b48acced
kaduvizu <kaduvizu@hotmail.com>
2024-02-18 17:00:31 -0300

    First commit

---
+++ src/test/java/com/cvizu/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvizu.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
